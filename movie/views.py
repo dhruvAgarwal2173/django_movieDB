01d688d5b38d98b81fbb565183943cc9123f63b3
dhruvAgarwal2173 <dhruv.agarwal2173@gmail.com>
2020-04-11 23:32:50 +0530

    added skeletal navbar

--- movie/views.py
+++ movie/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Movie
 from .forms import MovieForm
 from django.contrib import messages
@@ -14,19 +14,27 @@ def movie_add_view(request):
         # print('\n\n********************')
         # print(form)
     form = MovieForm
-    form_context = {
+    context = {
         'movie' : form
         }
     messages.success(request, 'Movie has been added!')
-    return render(request, 'movie/movie_add.html', form_context)
+    return render(request, 'movie/movie_add.html', context)
 
 
 def movie_detail_view(request):
     movie = Movie.objects.all()
-    movie_detail_context = {
+    context = {
         'movie': movie
     }
-    return render(request, 'movie/movie_detail.html', movie_detail_context)
+    return render(request, 'movie/movie_detail.html', context)
 
 def movie_home_view(request):
     return render(request, 'movie/movie_home.html', {})
+
+def movie_list_view(request):
+    movie = Movie.objects.all()
+    context = {
+        'movie' : movie
+    }
+    return render(request, 'movie/movie_list.html', context)
+
